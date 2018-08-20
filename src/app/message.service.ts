8534fa68b33fc7fffca2417262f4c004cd96f052
Ian Henderson <iancurtish@gmail.com>
2018-08-19 20:13:48 -0500

    formatting

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class MessageService {
-
   messages: string[] = [];
 
   constructor() { }
